4a37ce4fefc89d710ef5145f459f0c7365413cae
Faridkhan992 <70573374+Faridkhan992@users.noreply.github.com>
2021-01-06 12:43:26 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+
+
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    })
+
+// typing Animation
+var typed = new Typed(".typing",{
+    strings:["Developer.","Freelencer.","Designer."],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    // $(window).scroll(function(){
+    //     if(this.scrollY > 20){
+    //         $('.navbar').addClass("sticky");
+    //     }else{
+    //         $('.navbar').removeClass("sticky");
+    //     }
+    // })
+
+
+ 
+    // $(window).scroll(function(){
+    //     if(this.scrollY > 25){
+    //         $('.navbar').addClass("sticky2");
+    //     }else{
+    //         $('.navbar').removeClass("sticky2");
+    //     }
+    // })
+
